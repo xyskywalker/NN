2c391ae7e11346f6d852fede3f42ef83744a6517
Xing Yi <xyplus@live.com>
2017-03-15 10:42:23 +0800

    Update

--- 5_CNN_CIFAR10.py
+++ 5_CNN_CIFAR10.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 # 卷积网络 - CIFAR10 数据集分类
 
-from models.tutorials.image.cifar10 import cifar10,cifar10_input
+import cifar10,cifar10_input
 import tensorflow as tf
 import numpy as np
 import time
